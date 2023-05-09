fbb3be6618c6b03dc83092795b8c413eb9fe65f0
kaisahakola <97963741+kaisahakola@users.noreply.github.com>
2023-05-09 13:09:40 +0300

    Delete: parseJson method from FetchAll
    Alamofire's responseDecodable does the parsing automatically.

--- iOSProject/FetchAll.swift
+++ iOSProject/FetchAll.swift
@@ -8,17 +8,7 @@
 import Foundation
 import Alamofire
 
-func parseJson(json: Data) -> Array<User>? {
-    do {
-        let jsonDecoder = JSONDecoder()
-        let httpResult : Result = try jsonDecoder.decode(Result.self, from: json)
-        return httpResult.users
-    } catch {
-        return nil
-    }
-}
-
-func fetchData(callback : @escaping (_ users: Array<User>?) -> Void) {    
+func fetchData(callback : @escaping (_ users: Array<User>?) -> Void) {
     AF.request("https://dummyjson.com/users").responseDecodable(of: Result.self) { response in
         switch response.result {
         case .success(let value):
