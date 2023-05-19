7fd0f48a423abe43c18040e3c60e3d0cf45b2850
kaisahakola <97963741+kaisahakola@users.noreply.github.com>
2023-05-19 02:42:42 -0700

    Modify: Add user feature
    Adding an alert with TextFields to Add User button and calling the addUser function from the alert. 
    Extra: adding marks to the file to help organize the code.

--- iOSProject/ContentView.swift
+++ iOSProject/ContentView.swift
@@ -8,19 +8,27 @@
 import SwiftUI
 import Alamofire
 
+// MARK: - ContentView
+
+/// The main view of the iOS app.
 struct ContentView: View {
+    
+    // MARK: - Properties
     
     @State var users : Array<User>? = nil
     @State private var searchText = ""
     @State private var showDeletePopup = false
+    @State private var showAddUserPopup = false
+    @State private var newFirstName : String = ""
+    @State private var newLastName : String = ""
+    
+    // MARK: - Body
     
     var body: some View {
         if(users != nil) {
             NavigationView {
                 NavigationStack {
                     List {
-                        // Going trough the users and adding their names and
-                        // delete buttons for each user
                         ForEach(searchUser(search: searchText, users: users!),
                                 id: \.firstName) { user in
                             HStack {
@@ -38,25 +46,46 @@ struct ContentView: View {
                             }
                         }
                     }
-                    // Navigation link to open a new View for adding a new user
-                    NavigationLink(destination: AddUserView()) {
-                        Text("Add user")
-                    }
                     .navigationTitle("Kaisa's iOS app")
-                }
-                // Search bar for searching users
-                .searchable(text: $searchText)
-                // Pop up alert that shows when delete user button is pressed
-                .alert(isPresented: $showDeletePopup) {
-                    Alert(title: Text("User deleted"),
-                          dismissButton: .default(Text("OK")))
+                    
+                    // MARK: - Add user
+                    
+                    Button(action: {
+                        showAddUserPopup = true
+                    }) {
+                        Label("Add User", systemImage: "person.badge.plus")
+                    }
+                    .padding(15)
                 }
                 
+                // MARK: - Search
+                
+                .searchable(text: $searchText)
+                
+                // MARK: - Alerts
+                
+                .alert("User deleted", isPresented: $showDeletePopup, actions: {
+                    Button("OK", action: {
+                        showDeletePopup = false
+                    })
+                })
+                
+                .alert("Add user", isPresented: $showAddUserPopup, actions: {
+                    TextField("Fist name", text: $newFirstName)
+                    TextField("Last name", text: $newLastName)
+                    
+                    Button("Add", action: {
+                        addUser(firstName: newFirstName, lastName: newLastName)
+                    })
+                    Button("Cancel", role: .cancel, action: {
+                        showAddUserPopup = false
+                    })
+                })
             }
         } else {
-            // When ProgressView appears, fetchData() is called which gets
-            // data from dummyjson.com and sets the 'users' array to the
-            // array of 'User' objects returned by the function.
+            
+            // MARK: - Data fetching
+            
             ProgressView()
                 .onAppear() {
                     fetchData() {
